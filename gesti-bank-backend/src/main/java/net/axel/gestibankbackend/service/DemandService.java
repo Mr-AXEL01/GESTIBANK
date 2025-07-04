2e6b8c95ac714d6038d1f27b71d8430ecb9c60da
abdelhak20azur02@gmail.com <abdelhak20azur02@gmail.com>
2025-07-04 22:58:26 +0100

    set get all demands

--- gesti-bank-backend/src/main/java/net/axel/gestibankbackend/service/DemandService.java
+++ gesti-bank-backend/src/main/java/net/axel/gestibankbackend/service/DemandService.java
@@ -3,7 +3,10 @@ package net.axel.gestibankbackend.service;
 import net.axel.gestibankbackend.domain.dtos.demand.requests.DemandRequestDTO;
 import net.axel.gestibankbackend.domain.dtos.demand.responses.DemandResponseDTO;
 
+import java.util.List;
+
 public interface DemandService {
 
     DemandResponseDTO create(DemandRequestDTO dto, String email);
+    List<DemandResponseDTO> findAll(int page, int size);
 }
