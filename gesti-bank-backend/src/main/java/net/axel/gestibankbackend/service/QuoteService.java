b1c912cf35130b28b1b13be9fc2a344a8c638165
abdelhak20azur02@gmail.com <abdelhak20azur02@gmail.com>
2025-07-14 07:09:01 +0100

    set the updtae of the quote + change the validate url

--- gesti-bank-backend/src/main/java/net/axel/gestibankbackend/service/QuoteService.java
+++ gesti-bank-backend/src/main/java/net/axel/gestibankbackend/service/QuoteService.java
@@ -2,6 +2,7 @@ package net.axel.gestibankbackend.service;
 
 import net.axel.gestibankbackend.domain.dtos.quote.requests.QuoteManageDTO;
 import net.axel.gestibankbackend.domain.dtos.quote.requests.QuoteRequestDTO;
+import net.axel.gestibankbackend.domain.dtos.quote.requests.QuoteUpdateDTO;
 import net.axel.gestibankbackend.domain.dtos.quote.requests.QuoteValidateDTO;
 import net.axel.gestibankbackend.domain.dtos.quote.responses.QuoteResponseDTO;
 
@@ -11,7 +12,11 @@ public interface QuoteService {
 
     QuoteResponseDTO create(QuoteRequestDTO dto, String email);
 
+    QuoteResponseDTO update(QuoteUpdateDTO dto);
+
     List<QuoteResponseDTO> findAllQuotes(int page, int size);
+
+    QuoteResponseDTO findById(Long id);
 
     QuoteResponseDTO validate(QuoteValidateDTO dto, String email);
 
